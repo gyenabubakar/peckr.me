a7be9b60e3f18ed9dd8ab6184dfc22352034bf7d
Gyen Abubakar <gyenabubakar@gmail.com>
2025-11-02 13:15:05 +0000

    chore: update favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -78,13 +78,12 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
       },
       {
         rel: 'stylesheet',
-        // href: 'https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&display=swap',
         href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap',
       },
       {
         rel: 'icon',
         type: 'image/svg',
-        href: '/logo.svg',
+        href: '/favicon.svg',
       },
     ],
   }),
